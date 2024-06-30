075abf9a55bd68c358846c70e72d9403ea3e1d93
Elena <hel.lena84@mail.ru>
2024-06-30 15:08:57 +0400

    sem 4 start

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Обычно применяется следующий подход. Сначала определяется метод Main (главный метод),
+// внутри которого другие фунцкии обычно и вызываются. А после него уже определяются все другие (доп) функции
+void Main()
+{
+    PrintText();
+
+    System.Console.WriteLine();
+    System.Console.WriteLine("Example");
+    System.Console.WriteLine();
+
+    PrintText();
+}
+
+void PrintText()
+{
+    System.Console.WriteLine("Hello, world!");
+    System.Console.WriteLine("Hi there!");
+    System.Console.WriteLine("It's me!");
+}
+
+Main();
