e2710f82519c2d23d54bc87eb457f78edf3e77c5
Elena <hel.lena84@mail.ru>
2024-07-06 20:02:26 +0400

    seminar 6

---
+++ Seminar_6/Sem_6_Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿/ Задайте массив символов(тип char[]). Создайте строку из
+// символов этого массива.
+// Указание
+// Конструктор строки вида string(char []) не использовать.
+// Пример
+// [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+void Main()
+{
+char[] chars = { 'a', 'b', 'c', 'd' };
+
+// 1-й способ
+// string str = new string(chars);
+// System.Console.WriteLine(str);
+
+// 2-й способ
+// System.Console.WriteLine(CharsToString(chars)) ;
+
+// 3-й способ
+// System.Console.WriteLine(string.Join("", chars));
+
+// 4-й способ
+System.Console.WriteLine(string.Concat(chars));
+
+}
+
+// string CharsToString(char[] letters)
+// {
+// string str = ""; // не то же самое, что и null
+
+// foreach (char letter in letters)
+// str += letter;
+
+// return str;
+// }
+
+Main();
---
+++ Seminar_6/Sem_6_Task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+void Main()
+{
+    System.Console.Write("Ведите строку: ");
+    string? str = Console.ReadLine();
+    System.Console.WriteLine(CountVowels(str));
+    // TEXT
+}
+
+int CountVowels(string? str)
+{
+    int count = 0; // 0 -> 1
+
+    str = str!.ToLower(); // "TEXT" -> "text"
+
+    foreach (char letter in str) // letter = 't'
+        if (isVowel(letter))
+            count++;
+
+    return count;
+}
+
+// 'x'
+bool isVowel(char c)
+{
+    return "aeiouy".Contains(c); // c = x -> false
+}
+
+Main();
